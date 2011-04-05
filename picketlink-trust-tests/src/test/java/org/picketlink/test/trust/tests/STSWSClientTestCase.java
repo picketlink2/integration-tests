50b3850d292b2ef7e21753419c426f23016572c1
Anil Saldhana <anil.saldhana@jboss.com>
2011-04-05 17:20:52 +0000

    trust tests
    git-svn-id: https://svn.jboss.org/repos/picketlink/integration-tests/trunk@862 cbddccc4-e45a-42eb-be77-0d76e4b45d38

---
+++ picketlink-trust-tests/src/test/java/org/picketlink/test/trust/tests/STSWSClientTestCase.java
@@ -0,0 +1,82 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors. 
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.picketlink.test.trust.tests;
+
+import java.net.URL;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Service;
+import javax.xml.ws.handler.Handler;
+
+import org.junit.Test;
+import org.picketlink.identity.federation.api.wstrust.WSTrustClient;
+import org.picketlink.identity.federation.api.wstrust.WSTrustClient.SecurityInfo;
+import org.picketlink.identity.federation.core.wstrust.WSTrustException;
+import org.picketlink.identity.federation.core.wstrust.plugins.saml.SAMLUtil;
+import org.picketlink.test.trust.ws.WSTest;
+import org.picketlink.trust.jbossws.SAML2Constants;
+import org.picketlink.trust.jbossws.handler.SAML2Handler;
+import org.w3c.dom.Element;
+
+/**
+ * A Simple WS Test for the SAML Profile of WSS
+ * @author Marcus Moyses
+ * @author Anil Saldhana
+ * @since Oct 3, 2010
+ */
+public class STSWSClientTestCase
+{
+   private static String username = "UserA";
+   private static String password = "PassA";
+   
+   @SuppressWarnings("rawtypes")
+   @Test
+   public void testWSInteraction() throws Exception {
+      WSTrustClient client = new WSTrustClient("PicketLinkSTS", "PicketLinkSTSPort",
+            "http://localhost:8080/picketlink-sts/PicketLinkSTS",
+            new SecurityInfo(username, password));
+    Element assertion = null;
+    try {
+        System.out.println("Invoking token service to get SAML assertion for " + username);
+        assertion = client.issueToken(SAMLUtil.SAML2_TOKEN_TYPE);
+        System.out.println("SAML assertion for " + username + " successfully obtained!");
+    } catch (WSTrustException wse) {
+        System.out.println("Unable to issue assertion: " + wse.getMessage());
+        wse.printStackTrace();
+        System.exit(1);
+    }
+
+    URL wsdl = new URL("http://localhost:8080/picketlink-wstest-tests/WSTestBean?wsdl");
+    QName serviceName = new QName("http://ws.trust.test.picketlink.org/", "WSTestBeanService");
+    Service service = Service.create(wsdl, serviceName);
+    WSTest port = service.getPort(new QName("http://ws.trust.test.picketlink.org/", "WSTestBeanPort"), WSTest.class);
+    BindingProvider bp = (BindingProvider)port;
+    bp.getRequestContext().put(SAML2Constants.SAML2_ASSERTION_PROPERTY, assertion);
+    List<Handler> handlers = bp.getBinding().getHandlerChain();
+    handlers.add(new SAML2Handler());
+    bp.getBinding().setHandlerChain(handlers); 
+    
+    port.echo("Test");
+   }
+}
---
+++ picketlink-trust-tests/src/test/java/org/picketlink/test/trust/ws/WSTest.java
@@ -0,0 +1,37 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors. 
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.picketlink.test.trust.ws;
+
+import javax.ejb.Remote;
+import javax.jws.WebService;
+ 
+@Remote
+@WebService
+/**
+ * A Simple EJB3 Interface that just echoes a string
+ * @author Marcus Moyses
+ * @since Oct 3, 2010
+ */
+public interface WSTest
+{
+   public void echo(String echo);
+}
