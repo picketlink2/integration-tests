467f461540c59083e6dc6a6b54496ad1544a849d
Anil Saldhana <anil.saldhana@jboss.com>
2011-04-06 16:27:06 +0000

    add comments
    git-svn-id: https://svn.jboss.org/repos/picketlink/integration-tests/trunk@867 cbddccc4-e45a-42eb-be77-0d76e4b45d38

--- picketlink-trust-tests/src/test/java/org/picketlink/test/trust/tests/STSWSClientTestCase.java
+++ picketlink-trust-tests/src/test/java/org/picketlink/test/trust/tests/STSWSClientTestCase.java
@@ -49,34 +49,38 @@ public class STSWSClientTestCase
 {
    private static String username = "UserA";
    private static String password = "PassA";
-   
+
    @SuppressWarnings("rawtypes")
    @Test
-   public void testWSInteraction() throws Exception {
+   public void testWSInteraction() throws Exception 
+   {
+      // Step 1:  Get a SAML2 Assertion Token from the STS
       WSTrustClient client = new WSTrustClient("PicketLinkSTS", "PicketLinkSTSPort",
             "http://localhost:8080/picketlink-sts/PicketLinkSTS",
             new SecurityInfo(username, password));
-    Element assertion = null;
-    try {
-        System.out.println("Invoking token service to get SAML assertion for " + username);
-        assertion = client.issueToken(SAMLUtil.SAML2_TOKEN_TYPE);
-        System.out.println("SAML assertion for " + username + " successfully obtained!");
-    } catch (WSTrustException wse) {
-        System.out.println("Unable to issue assertion: " + wse.getMessage());
-        wse.printStackTrace();
-        System.exit(1);
-    }
+      Element assertion = null;
+      try {
+         System.out.println("Invoking token service to get SAML assertion for " + username);
+         assertion = client.issueToken(SAMLUtil.SAML2_TOKEN_TYPE);
+         System.out.println("SAML assertion for " + username + " successfully obtained!");
+      } catch (WSTrustException wse) {
+         System.out.println("Unable to issue assertion: " + wse.getMessage());
+         wse.printStackTrace();
+         System.exit(1);
+      }
 
-    URL wsdl = new URL("http://localhost:8080/picketlink-wstest-tests/WSTestBean?wsdl");
-    QName serviceName = new QName("http://ws.trust.test.picketlink.org/", "WSTestBeanService");
-    Service service = Service.create(wsdl, serviceName);
-    WSTest port = service.getPort(new QName("http://ws.trust.test.picketlink.org/", "WSTestBeanPort"), WSTest.class);
-    BindingProvider bp = (BindingProvider)port;
-    bp.getRequestContext().put(SAML2Constants.SAML2_ASSERTION_PROPERTY, assertion);
-    List<Handler> handlers = bp.getBinding().getHandlerChain();
-    handlers.add(new SAML2Handler());
-    bp.getBinding().setHandlerChain(handlers); 
-    
-    port.echo("Test");
+      // Step 2: Stuff the Assertion on the SOAP message context and add the SAML2Handler to client side handlers
+      URL wsdl = new URL("http://localhost:8080/picketlink-wstest-tests/WSTestBean?wsdl");
+      QName serviceName = new QName("http://ws.trust.test.picketlink.org/", "WSTestBeanService");
+      Service service = Service.create(wsdl, serviceName);
+      WSTest port = service.getPort(new QName("http://ws.trust.test.picketlink.org/", "WSTestBeanPort"), WSTest.class);
+      BindingProvider bp = (BindingProvider)port;
+      bp.getRequestContext().put(SAML2Constants.SAML2_ASSERTION_PROPERTY, assertion);
+      List<Handler> handlers = bp.getBinding().getHandlerChain();
+      handlers.add(new SAML2Handler());
+      bp.getBinding().setHandlerChain(handlers); 
+
+      //Step 3: Access the WS. Exceptions will be thrown anyway.
+      port.echo("Test");
    }
 }
