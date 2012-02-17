b27fab07d4eee62484c003d0d35619733f45a181
Anil Saldhana <anil.saldhana@jboss.com>
2012-02-17 19:59:34 +0000

    as71
    git-svn-id: https://svn.jboss.org/repos/picketlink/integration-tests/trunk@1408 cbddccc4-e45a-42eb-be77-0d76e4b45d38

--- common-dist/scripts/stopjboss.sh
+++ common-dist/scripts/stopjboss.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 ps -u`whoami` -opid,comm,args | grep "org.jboss.Main" | awk '{ print $1 }' | xargs kill -9
+
+ps -u`whoami` -opid,comm,args | grep "org.jboss.as.standalone" | awk '{ print $1 }' | xargs kill -9
